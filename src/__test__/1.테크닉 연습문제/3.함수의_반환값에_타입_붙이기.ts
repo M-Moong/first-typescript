7faf93793089bf95fba4504195527c304bef4125
Moong <tmdals9154@gmail.com>
2023-08-26 16:23:48 +0900

    타입스크립트 예제

---
+++ src/__test__/1.테크닉 연습문제/3.함수의_반환값에_타입_붙이기.ts
@@ -0,0 +1,43 @@
+export {};
+
+type Comment = {
+  id: string;
+  content: string;
+};
+
+interface User {
+  id: string;
+  name: string;
+  comment: Comment[];
+}
+
+/**
+ * 아래 두 함수의 반환값에 올바른 타입 어노테이션을 붙여 보세요.
+ */
+const createUser = (): User => {
+  return {
+    id: "0",
+    name: "0",
+    comment: [],
+  };
+};
+
+function createUser2(): User {
+  return {
+    id: "아이디",
+    name: "이름",
+    comment: [
+      {
+        id: "1",
+        content: "나나나",
+      },
+      {
+        id: "2",
+        content: "나나나",
+      },
+    ],
+  };
+}
+
+createUser();
+createUser2();
